0701b4a8f7035368129a6ba3d347c460cce2dbe0
allia-wase <a.umutoniwa1@alustudent.com>
2025-11-25 15:00:52 -0800

    add a main.jsx file

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,17 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
-import { AuthProvider } from './contexts/AuthContext'
-import './index.css'  // Make sure this line is here
-import './App.css'    // Add this line
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>,
 )
